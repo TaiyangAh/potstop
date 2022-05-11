83783faa7823652ad447e09610268c93e1cefd86
taiyangAh <wulian404@gmail.com>
2022-05-11 21:01:48 +0800

    adding ENS names and avatars

--- components/EthName.jsx
+++ components/EthName.jsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import { web3 } from "../lib/web3";
 import Jazzicon, { jsNumberForAddress } from "react-jazzicon";
+import ENS, { getEnsAddress } from "@ensdomains/ensjs";
+
+const ens = new ENS({
+  provider: web3.currentProvider,
+  ensAddress: getEnsAddress("1"),
+});
 
 const EnsName = function ({ address }) {
   // TODO!
@@ -10,19 +16,48 @@ const EnsName = function ({ address }) {
   // get image if it has one
   // make jazzicon if not
 
+  const [name, setName] = useState();
+  const [avatar, setAvatar] = useState();
+
+  useEffect(
+    async function () {
+      const n = await ens.getName(address);
+      if (n.name) {
+        setName(n.name);
+      }
+    },
+    [address]
+  );
+
+  useEffect(
+    async function () {
+      if (name) {
+        const a = await ens.name(name).getText("avatar");
+        if (a) {
+          setAvatar(a);
+        }
+      }
+    },
+    [name]
+  );
+
   let formattedAddress = address.substr(0, 6) + "..." + address.substr(-4);
 
   let icon = <Jazzicon diameter={32} seed={jsNumberForAddress(address)} />;
 
   return (
     <div className="eth-name">
-      <div className="icon">{icon}</div>
+      <div className="icon">{avatar ? <img src={avatar} /> : icon}</div>
 
       <div className="name">
-        <span className="primary">{/* ENS name if one here */}</span>
+        <span className="primary">
+          {name ? name : formattedAddress}
+          {/* ENS name if one here */}
+        </span>
 
         <span className="secondary">
-          {formattedAddress}
+          {name ? formattedAddress : ""}
+
           {/* formatted address here */}
         </span>
       </div>
